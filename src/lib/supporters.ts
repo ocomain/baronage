ecf328d878876e263f4fd5fb700e5659d76fe789
ddw <ddw@dontdiewondering.com>
2026-06-13 11:20:23 +0200

    Armorial: Rothes dignity Earl Baron -> Earl Lord Baron

--- src/lib/supporters.ts
+++ src/lib/supporters.ts
@@ -1,6 +1,6 @@
 /** The supporters shown in the homepage conveyor and on /supporters. */
 export type Barony = {
-  dignity: "Baron" | "Lord Baron" | "Earl Baron" | "Younger" | "Lord & Steward" | "Countess" | "Baroness";
+  dignity: "Baron" | "Lord Baron" | "Earl Lord Baron" | "Younger" | "Lord & Steward" | "Countess" | "Baroness";
   name: string;
   img?: string;
   note?: string;
@@ -40,7 +40,7 @@ export const baronies: Barony[] = [
   },
   { dignity: "Baron", name: "Mugdock", roll: "https://roll.baronage.com/baron/luciano-lambertini", img: "/arms/mugdock.webp" },
   { dignity: "Baron", name: "Hartsyde", roll: "https://roll.baronage.com/baron/jean-guy-philip-boisserolles-de-st-julien", img: "/arms/hartsyde.webp" },
-  { dignity: "Earl Baron", name: "Rothes", roll: "https://roll.baronage.com/dario-item.html", img: "/arms/rothes.webp", pageOnly: true },
+  { dignity: "Earl Lord Baron", name: "Rothes", roll: "https://roll.baronage.com/dario-item.html", img: "/arms/rothes.webp", pageOnly: true },
   { dignity: "Baron", name: "Stobo", roll: "https://roll.baronage.com/baron/william-jolly", img: "/arms/stobo.webp" },
   { dignity: "Baron", name: "Finavon", img: "/arms/finavon.webp", roll: "https://roll.baronage.com/baron/david-cairns", pageOnly: true },
   { dignity: "Younger", name: "Kinfauns", img: "/arms/kinfauns-arms.webp", pageOnly: true },
